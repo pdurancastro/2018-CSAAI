c460580020fe39167948dfc6c12ba7c5b9210923
Patricia <patryharuno@gmail.com>
2018-05-17 02:14:08 +0200

    El realizador ahora pilla repeticiones

--- Realizador de video/Realizador_Video.js
+++ Realizador de video/Realizador_Video.js
@@ -1,6 +1,7 @@
-
-
+var loop = 0;
+var counter = 0;
 function clickCambioVideo(event){
+    counter = 0;
     //VideoLib = document.getElementsByClassName("VideoSelect");
     //console.log(event.target)
     document.getElementById("Videobase").src = event.target.src;
@@ -21,13 +22,20 @@ function RatonSonido(event){
     document.getElementById("Video2").muted = "true";
     document.getElementById("Video3").muted = "true";
     document.getElementById("Video4").muted = "true";
-
     document.getElementById(event.target.id).muted = false;
+}
 
+function checkloop(){
+  var num_loop = document.getElementById("Repeticion").value;
+  if (num_loop != ''){
+    loop = num_loop;
+  }
 }
 
 //Debo ponerlo para cada segundo
 function checkTiempoVideo(){
+
+  checkloop();
   var video = document.getElementById("Videobase");
   document.getElementById("timer").innerHTML = "Timer:" + video.currentTime;
   //console.log(video.currentTime);
@@ -35,16 +43,23 @@ function checkTiempoVideo(){
   var inicio = document.getElementById("Inicio").value;
   var final = document.getElementById("Final").value;
   //console.log(inicio);
-
+  var repeticion = loop;
+  //console.log(repeticion);
   // Restriccion Video
-  if (inicio != '' && final != ''){
+
+  if (inicio != '' && final != '' && counter <= repeticion){
     if( video.currentTime > final){
       video.currentTime = inicio;
+      counter = counter + 1;
+      //repeticion = repeticion - 1;
     }else if( inicio > video.currentTime ){
       video.currentTime = inicio;
-    }
+      counter = counter + 1;
+      //repeticion = repeticion - 1;
+    }console.log(counter)
   }
 }
+
 
 
 function start(){
